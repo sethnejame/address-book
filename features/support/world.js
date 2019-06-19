5919d5d90a3bf6ac7bc3a510ed5597fa970c5265
Seth NeJame <sethnejame@gmail.com>
2019-06-19 13:29:17 +0200

    Adding faraz testing files, pushing to new branch for new tests

--- features/support/world.js
+++ features/support/world.js
@@ -1,3 +1,69 @@
+// const { setWorldConstructor } = require('cucumber')
+// const { expect } = require('chai')
+// const puppeteer = require('puppeteer')
+
+// const HOME_PAGE = 'http://localhost:3000'
+
+// class AddressBookWorld {
+//   constructor() {}
+
+//   // Open the home page using puppeteer
+//   async openHomePage() {
+//     this.browser = await puppeteer.launch({headless: false, slowmo: 100})
+//     this.page = await this.browser.newPage()
+//     await this.page.goto(HOME_PAGE)
+//   }
+//   // Close the home page
+//   async closeHomePage() {
+//     await this.browser.close()
+//   } // show contacts on h1
+//   async pageHasTextContent(expectedContent) {
+//     const pageContent = await this.page.content()
+//     const actualContent = pageContent.match(expectedContent)[0]
+
+//     expect(actualContent).to.be.eq(expectedContent)
+//   }
+//   async clickOnButton(btnName) {
+//     const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
+//     await this.page.waitForSelector(btnSelector)
+//     await this.page.click(btnSelector)
+//   }
+//   async fillFormField(field, content) {
+//     const inputSelector = `#contact-${field}`
+//     await this.page.waitForSelector(inputSelector)
+//     this.inputElement = await this.page.$(inputSelector)
+//     await this.inputElement.type(content)
+//   }
+//   btnSelectorFromName(btnName) {
+//     switch (btnName) {
+//       case 'add contact':
+//         return '.add-contact'
+//         break
+//       case 'save contact':
+//         return '.save-contact'
+//         break
+//       default:
+//         throw `${btnName} button is not defined`
+//         break
+//     }
+//   }
+//   async checkContactStorageCount(expectedCount) {
+//     const actualCount = await this.page.evaluate(
+//       () => JSON.parse(window.localStorage.getItem('contacts')).length
+//     )
+//     expect(actualCount).to.be.eq(expectedCount)
+//   }
+//   async pageDoesNotHaveTextContent(unexpectedContent) {
+//     const pageContent = await this.page.content()
+//     let actualContent = pageContent.match(unexpectedContent)
+
+//     expect(actualContent).to.be.eq(null)
+//   }
+// }
+
+
+// setWorldConstructor(AddressBookWorld)
+
 const { setWorldConstructor } = require('cucumber')
 const { expect } = require('chai')
 const puppeteer = require('puppeteer')
@@ -13,27 +79,45 @@ class AddressBookWorld {
     this.page = await this.browser.newPage()
     await this.page.goto(HOME_PAGE)
   }
-  // Close the home page
+
   async closeHomePage() {
     await this.browser.close()
-  } // show contacts on h1
+  }
+
   async pageHasTextContent(expectedContent) {
     const pageContent = await this.page.content()
     const actualContent = pageContent.match(expectedContent)[0]
 
     expect(actualContent).to.be.eq(expectedContent)
   }
+
   async clickOnButton(btnName) {
     const btnSelector = this.btnSelectorFromName(btnName.toLowerCase())
     await this.page.waitForSelector(btnSelector)
     await this.page.click(btnSelector)
   }
+
   async fillFormField(field, content) {
     const inputSelector = `#contact-${field}`
     await this.page.waitForSelector(inputSelector)
     this.inputElement = await this.page.$(inputSelector)
     await this.inputElement.type(content)
   }
+
+  async checkContactStorageCount(expectedCount) {
+    const actualCount = await this.page.evaluate(
+      () => JSON.parse(window.localStorage.getItem('contacts')).length
+    )
+    expect(actualCount).to.be.eq(expectedCount)
+  }
+
+  async pageDoesNotHaveTextContent(unexpectedContent) {
+    const pageContent = await this.page.content()
+    let actualContent = pageContent.match(unexpectedContent)
+
+    expect(actualContent).to.be.eq(null)
+  }
+
   btnSelectorFromName(btnName) {
     switch (btnName) {
       case 'add contact':
@@ -47,19 +131,6 @@ class AddressBookWorld {
         break
     }
   }
-  async checkContactStorageCount(expectedCount) {
-    const actualCount = await this.page.evaluate(
-      () => JSON.parse(window.localStorage.getItem('contacts')).length
-    )
-    expect(actualCount).to.be.eq(expectedCount)
-  }
-  async pageDoesNotHaveTextContent(unexpectedContent) {
-    const pageContent = await this.page.content()
-    let actualContent = pageContent.match(unexpectedContent)
-
-    expect(actualContent).to.be.eq(null)
-  }
 }
-
 
 setWorldConstructor(AddressBookWorld)
