1260bbd389c71fb409f3974a6e40d245944cc295
Seth NeJame <sethnejame@gmail.com>
2019-06-19 10:00:20 +0200

    Adding working tests for renderContacts()

--- src/app.js
+++ src/app.js
@@ -1,4 +1,51 @@
+const renderContacts = () => {
+  const storage = window.localStorage
+  // Read all the contacts from the storage
+  const contacts = JSON.parse(storage.getItem('contacts'))
+
+  // Select the container we will use to list the contacts 
+  let div = document.querySelector('.contact-list')
+
+  if (contacts) {
+    div.innerHTML = ''
+
+    // render the contacts
+    const ul = document.createElement('ul')
+
+    // For every contact in our array of contacts, we will
+    // create a li element that will contain a card with
+    // all the information of the contact
+    contacts.forEach(contact => {
+      let li = document.createElement('li')
+      li.innerHTML = `
+        <div class="card">
+          <div class="image">
+            <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
+          </div>
+          <div class="content">
+            <h1>${ contact.name }</h1>
+            <h2>${ contact.company }</h2>
+            <p>${ contact.notes }</p> 
+            ${ contact.email } | 
+            <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+          </div>
+        </div>
+     `
+      // Add the contact's li to the unordered list we created earlier
+      ul.appendChild(li)
+    })
+
+    // Lastly, append the list to the contact-list container.
+    div.appendChild(ul) 
+  } else { 
+    div.innerHTML = '<p>You have no contacts in your address book</p>' 
+  }
+}
+
+
 document.addEventListener('DOMContentLoaded', () => {
+    // As soon as the page is loaded, run render Contacts to load the page up with saved contacts 
+    renderContacts()
     // Select form object from the DOM
     const addContactForm = document.querySelector('.new-contact-form')
   
@@ -29,5 +76,7 @@ document.addEventListener('DOMContentLoaded', () => {
       console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
       
       storage.setItem('contacts', JSON.stringify([contact]))
+      // Run render contacts one more time after we add a new one, to update the list
+      renderContacts()
     })
   })  
