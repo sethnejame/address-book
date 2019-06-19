d5efe1ce5131ab3f78af1002242462e3d32d3a8c
Seth NeJame <sethnejame@gmail.com>
2019-06-19 13:51:19 +0200

    Adding all tests GO

--- features/step_definitions/basic-steps.js
+++ features/step_definitions/basic-steps.js
@@ -1,61 +1,31 @@
-// const { After, Given, Then, When } = require('cucumber')
-// //this works
-// Given('I visit the site', async function() {
-//   return await this.openHomePage()
-// })
-// //this works
-// After(async function() {
-//   return await this.closeHomePage()
-// })
-// //this works
-// Then('I should see {string}', async function(content) {
-//   return await this.pageHasTextContent(content)
-  
-// })
-// //this works
-// When('I click {string}', async function(string) {
-//   return await this.clickOnButton(string)
-  
-// })
-// //this works
-// Then('I fill in {string} with {string}', async function(field, content) {
-//   return await this.fillFormField(field.toLowerCase(), content)
-// })
-
-// Then('I should have {int} contact in my address book', async function (contactCount) {
-//   return await this.checkContactStorageCount(contactCount)
-// })
-
-// Then('I should not see {string}', async function (content) {
-//   return await this.pageDoesNotHaveTextContent(content)
-// })
-
 const { After, Given, Then, When } = require('cucumber')
-
-After(async function() {
-    return await this.closeHomePage()
-  })
-
+//this works
 Given('I visit the site', async function() {
-    return await this.openHomePage()
+  return await this.openHomePage()
 })
-
+//this works
+After(async function() {
+  return await this.closeHomePage()
+})
+//this works
 Then('I should see {string}', async function(content) {
-    return await this.pageHasTextContent(content)
+  return await this.pageHasTextContent(content)
+  
 })
-
+//this works
 When('I click {string}', async function(string) {
-    return await this.clickOnButton(string)
+  return await this.clickOnButton(string)
+  
 })
-
+//this works
 Then('I fill in {string} with {string}', async function(field, content) {
-    return await this.fillFormField(field.toLowerCase(), content)
+  return await this.fillFormField(field.toLowerCase(), content)
 })
 
-Then('I should have {int} contact in my address book', async function(contactCount) {
-    return await this.checkContactStorageCount(contactCount)
+Then('I should have {int} contact in my address book', async function (contactCount) {
+  return await this.checkContactStorageCount(contactCount)
 })
 
-Then('I should not see {string}', async function(content) {
-    return await this.pageDoesNotHaveTextContent(content)
+Then('I should not see {string}', async function (content) {
+  return await this.pageDoesNotHaveTextContent(content)
 })
--- src/app.js
+++ src/app.js
@@ -1,151 +1,88 @@
-// const storage = window.localStorage
-
-// const renderContacts = () => {
-  
-//   // Read all the contacts from the storage
-//   const contacts = JSON.parse(storage.getItem('contacts'))
-
-//   // Select the container we will use to list the contacts 
-//   let div = document.querySelector('.contact-list')
-
-//   if (contacts) {
-//     div.innerHTML = ''
-
-//     // render the contacts
-//     const ul = document.createElement('ul')
-
-//     // For every contact in our array of contacts, we will
-//     // create a li element that will contain a card with
-//     // all the information of the contact
-//     contacts.forEach(contact => {
-//       let li = document.createElement('li')
-//       li.innerHTML = `
-//         <div class="card">
-//           <div class="image">
-//             <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
-//           </div>
-//           <div class="content">
-//             <h1>${ contact.name }</h1>
-//             <h2>${ contact.company }</h2>
-//             <p>${ contact.notes }</p> 
-//             ${ contact.email } | 
-//             <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
-//           </div>
-//         </div>
-//      `
-//       // Add the contact's li to the unordered list we created earlier
-//       ul.appendChild(li)
-//     })
-
-//     // Lastly, append the list to the contact-list container.
-//     div.appendChild(ul) 
-//   } else { 
-//     div.innerHTML = '<p>You have no contacts in your address book</p>' 
-//   }
-// }
-
-
-// document.addEventListener('DOMContentLoaded', () => {
-//     // As soon as the page is loaded, run render Contacts to load the page up with saved contacts 
-//     renderContacts()
-//     // Select form object from the DOM
-//     const addContactForm = document.querySelector('.new-contact-form')
-  
-//     // Register an event to listen for form submission
-//     addContactForm.addEventListener('submit', event => {
-//       event.preventDefault()
-//       //const storage = window.localStorage
-//       const {
-//         name,
-//         email,
-//         phone,
-//         company,
-//         notes,
-//         twitter,
-//       } = addContactForm.elements
-  
-//       // Create contact object
-//       const contact = {
-//         id: Date.now(),
-//         name: name.value,
-//         email: email.value,
-//         phone: phone.value,
-//         company: company.value,
-//         notes: notes.value,
-//         twitter: twitter.value,
-//       }
-  
-//       console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
-
-//       let contacts = JSON.parse(storage.getItem('contacts')) || []
-
-//       contacts.push(contact)
-//       // store existing contact currently added
-//       storage.setItem('contacts', JSON.stringify(contacts))
-//       renderContacts()
-//       addContactForm.reset()
-//     })
-//   })  
-
-
 const storage = window.localStorage
 
 const renderContacts = () => {
+  
   // Read all the contacts from the storage
   const contacts = JSON.parse(storage.getItem('contacts'))
 
+  // Select the container we will use to list the contacts 
   let div = document.querySelector('.contact-list')
 
   if (contacts) {
     div.innerHTML = ''
+
     // render the contacts
     const ul = document.createElement('ul')
 
+    // For every contact in our array of contacts, we will
+    // create a li element that will contain a card with
+    // all the information of the contact
     contacts.forEach(contact => {
       let li = document.createElement('li')
-
       li.innerHTML = `
-        <span>${contact.name}</span> |
-        <span>${contact.email}</span> |
-        <span>${contact.phone}</span>
-      `
+        <div class="card">
+          <div class="image">
+            <img src="https://ca-address-book.herokuapp.com/images/pine.jpg" />
+          </div>
+          <div class="content">
+            <h1>${ contact.name }</h1>
+            <h2>${ contact.company }</h2>
+            <p>${ contact.notes }</p> 
+            ${ contact.email } | 
+            <a href="https://www.twitter.com/${ contact.twitter}">@${contact.twitter}</a>
+          </div>
+        </div>
+     `
+      // Add the contact's li to the unordered list we created earlier
       ul.appendChild(li)
     })
 
-    div.appendChild(ul)
-  } else {
-    div.innerHTML = '<p>You have no contacts in your address book</p>'
+    // Lastly, append the list to the contact-list container.
+    div.appendChild(ul) 
+  } else { 
+    div.innerHTML = '<p>You have no contacts in your address book</p>' 
   }
 }
 
+
 document.addEventListener('DOMContentLoaded', () => {
-  renderContacts()
-  const contactForm = document.querySelector('.new-contact-form')
-
-  contactForm.addEventListener('submit', event => {
-    event.preventDefault()
-
-    // 1. Read all the input fields and get their values
-    const { name, email, phone, company, notes, twitter } = contactForm.elements
-
-    const contact = {
-      name: name.value,
-      email: email.value,
-      phone: phone.value,
-      company: company.value,
-      notes: notes.value,
-      twitter: twitter.value,
-    }
-
-    console.log(contact)
-
-    let contacts = JSON.parse(storage.getItem('contacts')) || []
-
-    contacts.push(contact)
-
-    // 2. Save them to our storage
-    storage.setItem('contacts', JSON.stringify(contacts))
+    // As soon as the page is loaded, run render Contacts to load the page up with saved contacts 
     renderContacts()
-    contactForm.reset()
-  })
-})
+    // Select form object from the DOM
+    const addContactForm = document.querySelector('.new-contact-form')
+  
+    // Register an event to listen for form submission
+    addContactForm.addEventListener('submit', event => {
+      event.preventDefault()
+      //const storage = window.localStorage
+      const {
+        name,
+        email,
+        phone,
+        company,
+        notes,
+        twitter,
+      } = addContactForm.elements
+  
+      // Create contact object
+      const contact = {
+        id: Date.now(),
+        name: name.value,
+        email: email.value,
+        phone: phone.value,
+        company: company.value,
+        notes: notes.value,
+        twitter: twitter.value,
+      }
+  
+      console.log(`Saving the following contact: ${JSON.stringify(contact)}`)
+
+      let contacts = JSON.parse(storage.getItem('contacts')) || []
+
+      contacts.push(contact)
+      // store existing contact currently added
+      storage.setItem('contacts', JSON.stringify(contacts))
+      renderContacts()
+      addContactForm.reset()
+    })
+  })  
